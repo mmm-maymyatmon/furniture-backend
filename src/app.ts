520882aa4b7e178a5c3a8d4de7522053ba8ef056
mmm-maymyatmon <maymyat.dev@gmail.com>
2025-04-28 21:17:24 +0700

    ➕ Database migration
    database migration

---
+++ src/app.ts
@@ -0,0 +1,27 @@
+import express, { NextFunction, Request, Response } from "express";
+import helmet from "helmet";
+import compression from "compression";
+import cors from "cors";
+import morgan from "morgan";
+import limiter from "./middlewares/rateLimiter";
+import healthRoutes from "./routes/v1/health"
+
+export const app = express();
+
+app
+  .use(morgan("dev"))
+  .use(express.urlencoded({ extended: true }))
+  .use(express.json())
+  .use(cors())
+  .use(helmet())
+  .use(compression())
+  .use(limiter);
+
+app.use("/api/v1", healthRoutes)
+
+app.use((error: any, req: Request, res: Response, next: NextFunction) => {
+  const status = error.status || 500;
+  const message = error.message || "Server Error";
+  const errorCode = error.errorCode || "Error_Code";
+  res.status(status).json({ message, error: errorCode });
+});
