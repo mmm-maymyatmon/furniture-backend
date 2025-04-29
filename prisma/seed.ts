3c94864ce343f2486286be618757823a88428724
mmm-maymyatmon <maymyat.dev@gmail.com>
2025-04-29 14:05:04 +0700

    🤡 Faker.js
    faker.js

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -1,41 +1,54 @@
 import { PrismaClient, Prisma } from "../generated/prisma";
 import * as bcrypt from 'bcryptjs';
+import { faker } from '@faker-js/faker';
 
 const prisma = new PrismaClient();
 
-const userData: Prisma.UserCreateInput[] = [
-  {
-    phone: "1234567891",
-    password: "",
-    randToken: "sfrwerwehfdsaoiwr45345fndsapifoenwrew",
-  },
-  {
-    phone: "1234567892",
-    password: "",
-    randToken: "sfrwerwehfdsaoiwr45345fndsapifoenwrew",
-  },
-  {
-    phone: "1234567893",
-    password: "",
-    randToken: "sfrwerwehfdsaoiwr45345fndsapifoenwrew",
-  },
-  {
-    phone: "1234567894",
-    password: "",
-    randToken: "sfrwerwehfdsaoiwr45345fndsapifoenwrew",
-  },
-  {
-    phone: "1234567895",
-    password: "",
-    randToken: "sfrwerwehfdsaoiwr45345fndsapifoenwrew",
-  },
-];
+export function createRandomUser() {
+    return {
+      phone: faker.phone.number({style: 'international'}),
+      password: "",
+      randToken: faker.internet.jwt(),
+    };
+  }
+  
+  export const users = faker.helpers.multiple(createRandomUser, {
+    count: 10,
+  });
+
+// const userData: Prisma.UserCreateInput[] = [
+//   {
+//     phone: "1234567891",
+//     password: "",
+//     randToken: "sfrwerwehfdsaoiwr45345fndsapifoenwrew",
+//   },
+//   {
+//     phone: "1234567892",
+//     password: "",
+//     randToken: "sfrwerwehfdsaoiwr45345fndsapifoenwrew",
+//   },
+//   {
+//     phone: "1234567893",
+//     password: "",
+//     randToken: "sfrwerwehfdsaoiwr45345fndsapifoenwrew",
+//   },
+//   {
+//     phone: "1234567894",
+//     password: "",
+//     randToken: "sfrwerwehfdsaoiwr45345fndsapifoenwrew",
+//   },
+//   {
+//     phone: "1234567895",
+//     password: "",
+//     randToken: "sfrwerwehfdsaoiwr45345fndsapifoenwrew",
+//   },
+// ];
 
 async function main() {
   console.log(`Starting seed...`);
   const salt = await bcrypt.genSalt(10);
   const password = await bcrypt.hash("12345678", salt);
-  for (const u of userData) {
+  for (const u of users) {
     u.password = password;
     await prisma.user.create({ data: u });
   }
