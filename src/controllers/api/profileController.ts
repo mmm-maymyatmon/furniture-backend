d68b1600493fb03e1dc5b8265143c33c550fb6f6
mmm-maymyatmon <maymyat.dev@gmail.com>
2025-05-15 08:15:12 +0700

    🌱 Multer file filter
    Multer file filter

--- src/controllers/api/profileController.ts
+++ src/controllers/api/profileController.ts
@@ -1,9 +1,13 @@
 import { Request, Response, NextFunction } from "express";
 import { body, query, validationResult } from "express-validator";
 import { errorCode } from "../../../config/errorCode";
+import { getUserById } from "../../services/authService";
+import { checkUserExist, checkUserIfNotExist } from "../../utils/auth";
+import { checkUploadFile } from "../../utils/check";
 
 interface CustomRequest extends Request {
-  userId?: string;
+  userId?: number;
+  file?: any;
 }
 
 export const changeLanguage = [
@@ -25,3 +29,18 @@ export const changeLanguage = [
     res.cookie("i18next", lng);
     res.status(200).json({ message: req.t("changeLan", { lang: lng }) })
   }];
+
+  export const uploadProfile = async (req: CustomRequest, res: Response, next: NextFunction) => {
+    const userId = req.userId;
+    const image = req.file;  
+    const user = await getUserById(userId!);
+    checkUserIfNotExist(user);
+    checkUploadFile(image);
+
+
+
+    res.status(200).json({
+      message: "Profile image uploaded successfully"
+
+    })
+  }
--- src/middlewares/uploadFile.ts
+++ src/middlewares/uploadFile.ts
@@ -1,28 +1,46 @@
-import { Request, Response, NextFunction } from 'express';
-import multer, { FileFilterCallback } from 'multer';
+import { Request, Response, NextFunction } from "express";
+import multer, { FileFilterCallback } from "multer";
 
 const fileStorage = multer.diskStorage({
   destination: function (req, file, cb) {
     // cb(null, 'uploads/images')
-    const type = file.mimetype.split('/')[0];
-    if (type === 'image') {
-      cb(null, 'uploads/images');
-    } else if (type === 'video') {
-      cb(null, 'uploads/videos');
-    }
-    else if (type === 'audio') {
-      cb(null, 'uploads/audios');
+    const type = file.mimetype.split("/")[0];
+    if (type === "image") {
+      cb(null, "uploads/images");
+    } else if (type === "video") {
+      cb(null, "uploads/videos");
+    } else if (type === "audio") {
+      cb(null, "uploads/audios");
     } else {
-      cb(new Error('Invalid file type'), 'uploads/others');
+      cb(new Error("Invalid file type"), "uploads/others");
     }
   },
   filename: function (req, file, cb) {
-    const uniqueSuffix = Date.now() + '-' + Math.round(Math.random() * 1E9)
-    cb(null, uniqueSuffix + '-' + file.originalname)
-  }
+    const uniqueSuffix = Date.now() + "-" + Math.round(Math.random() * 1e9);
+    cb(null, uniqueSuffix + "-" + file.originalname);
+  },
 });
 
+const fileFilter = (
+  req: Request,
+  file: Express.Multer.File,
+  cb: FileFilterCallback
+) => {
+  if (
+    file.mimetype === "image/jpeg" ||
+    file.mimetype === "image/png" ||
+    file.mimetype === "image/jpg" 
+  ) {
+    cb(null, true);
+  } else {
+    cb(null, false);
+  }
+};
 
-const upload = multer({ storage: fileStorage })
+const upload = multer({
+  storage: fileStorage,
+  fileFilter: fileFilter,
+  limits: { fileSize: 1024 * 1024 * 5 }, //Testing 5MB
+});
 
-export default upload
+export default upload;
