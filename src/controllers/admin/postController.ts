8e4cd5025a03b0d582604eb1362e24092f0cb781
mmm-maymyatmon <maymyat.dev@gmail.com>
2025-05-17 09:23:15 +0700

    ⚡️ Sanitize HTML
    Sanitize HTML

--- src/controllers/admin/postController.ts
+++ src/controllers/admin/postController.ts
@@ -1,5 +1,6 @@
 import { Request, Response, NextFunction } from "express";
 import { body, validationResult } from "express-validator";
+import sanitizeHtml from "sanitize-html";
 import { errorCode } from "../../../config/errorCode";
 import { createError } from "../../utils/error";
 import { getUserById } from "../../services/authService";
@@ -7,15 +8,47 @@ import { checkUserIfNotExist } from "../../utils/auth";
 import { checkUploadFile } from "../../utils/check";
 import ImageQueue from "../../jobs/queues/imageQueue";
 import { createOnePost, PostArgs } from "../../services/postService";
+import path from "path";
+import { unlink } from "fs/promises";
 
 interface CustomRequest extends Request {
   userId?: number;
 }
 
+const removeFiles = async (originalFile: string, optimizedFile? : string | null ) => {
+    try {
+        const originalFilePath = path.join(
+            __dirname,
+            "../../../",
+             "uploads/images",
+            originalFile
+        );
+        
+        await unlink(originalFilePath);
+
+        if(optimizedFile) {
+            const optimizedFilePath = path.join(
+            __dirname,
+            "../../../",
+            "uploads/optimize",
+            originalFile
+        );
+        await unlink(optimizedFilePath);
+        }
+        
+    }
+    catch(error) {
+        console.log("Error deleting file: ", error);
+    }
+}
+
 export const createPost = [
   body("title", "Title is required").trim().notEmpty().escape(),
   body("content", "Content is required").trim().notEmpty().escape(),
-  body("body", "Body is required").trim().notEmpty().escape(),
+  body("body", "Body is required")
+    .trim()
+    .notEmpty()
+    .customSanitizer((value) => sanitizeHtml(value)).notEmpty(),
   body("category", "Category is required").trim().notEmpty().escape(),
   body("type", "Type is required").trim().notEmpty().escape(),
   body("tags", "Tag is invalid")
@@ -27,51 +60,61 @@ export const createPost = [
       return value;
     }),
 
-  
   async (req: CustomRequest, res: Response, next: NextFunction) => {
     const errors = validationResult(req).array({ onlyFirstError: true });
     if (errors.length > 0) {
-      return next(createError(errorCode.invalid, 400, errors[0].msg));
+        if(req.file) {
+            await removeFiles(req.file.filename, null);
+        }
+      return next(createError(errors[0].msg, 400, errorCode.invalid));
     }
     const { title, content, body, category, type, tags } = req.body;
     const userId = req.userId;
-      const image = req.file;
-      const user = await getUserById(userId!);
-      checkUserIfNotExist(user);
-      checkUploadFile(image);
-
-      const splitFileName = req.file?.filename.split(".")[0];
-
-      await ImageQueue.add(
-        "optimize-image",
-        {
-          filePath: req.file?.path,
-          fileName: `${splitFileName}.webp`,
-          width: 835,
-          height: 577,
-          quality: 100,
-        },
-        {
-          attempts: 3,
-          backoff: {
-            type: "exponential",
-            delay: 1000,
-          },
+    const image = req.file;
+    checkUploadFile(image);
+    const user = await getUserById(userId!);
+     if(!user) {
+        if(req.file) {
+            await removeFiles(req.file.filename, null);
         }
-      );
-      const data:PostArgs = {
-        title,
-        content,
-        body,
-        image: req.file!.filename,
-        authorId: user!.id,
-        category,
-        type,
-        tags
-      };
-      const post = await createOnePost(data);
+        return next(createError("This user has not registered.",401, errorCode.unauthenticated ));
+    }
+    
 
-    res.status(201).json({ message: "Successfully created new post.", postId: post.id });
+    const splitFileName = req.file?.filename.split(".")[0];
+
+    await ImageQueue.add(
+      "optimize-image",
+      {
+        filePath: req.file?.path,
+        fileName: `${splitFileName}.webp`,
+        width: 835,
+        height: 577,
+        quality: 100,
+      },
+      {
+        attempts: 3,
+        backoff: {
+          type: "exponential",
+          delay: 1000,
+        },
+      }
+    );
+    const data: PostArgs = {
+      title,
+      content,
+      body,
+      image: req.file!.filename,
+      authorId: user!.id,
+      category,
+      type,
+      tags,
+    };
+    const post = await createOnePost(data);
+
+    res
+      .status(201)
+      .json({ message: "Successfully created new post.", postId: post.id });
   },
 ];
 
