9fd4eb61683100a5dd6115d01e7b6321e507e434
mmm-maymyatmon <maymyat.dev@gmail.com>
2025-05-26 22:54:06 +0700

    ⚡️ Auth-check api
    auth-check api

--- src/routes/v1/auth.ts
+++ src/routes/v1/auth.ts
@@ -1,5 +1,6 @@
 import express from "express"
-import { register, verifyOtp, confirmPassword, login, logout, forgetPassword, resetPassword, verifyOtpForChangePassword } from "../../controllers/authController";
+import { register, verifyOtp, confirmPassword, login, logout, forgetPassword, resetPassword, verifyOtpForChangePassword, authCheck } from "../../controllers/authController";
+import { auth } from "../../middlewares/auth";
 
 const router = express.Router();
 
@@ -12,7 +13,7 @@ router.post("/logout", logout)
 router.post("/forget-password", forgetPassword)
 router.post("/reset-password", resetPassword)
 router.post("/verify-otp-for-change-password", verifyOtpForChangePassword)
-
+router.get("/auth-check", auth, authCheck)
 
 
 
